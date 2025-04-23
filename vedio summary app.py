661a40765764e4e11ba9b0d9a001b102e92ecbbb
Chethangowda369 <cc64776@gmail.com>
2025-04-23 15:04:43 +0530

    Update vedio summary app.py

--- vedio summary app.py
+++ vedio summary app.py
@@ -86,7 +86,7 @@ def turn_into_story(summary):
 
 # Streamlit UI
 st.title("📺Chetan - YouTube/Uploaded Video Summarizer Using Groq LLM")
-st.image("PragyanAI_Transperent.png")
+st.image("WhatsApp Image 2025-04-23 at 7.03.02 AM.jpeg")
 
 youtube_url = st.text_input("Paste a YouTube video URL:", placeholder="https://www.youtube.com/watch?v=example")
 
